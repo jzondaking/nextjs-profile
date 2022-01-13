6675d31f7554ffaf96c4bd9b294f14e2351399cd
Phạm Đức Thanh <phamducthanhpubg@gmail.com>
2022-01-13 13:40:42 +0700

    zzz

---
+++ pages/index.js
@@ -0,0 +1,247 @@
+import Script from 'next/script'
+
+export default function Home() {
+  return (
+    <div>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <title>Profile Bé Thanh Cutiez</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <meta name="description" content="Profile Bé Thanh Cutiez" />
+      <meta name="keywords" content="Phạm Đức Thanh, jzon, pham duc thanh, be thanh profile, nextjs, reactjs" />
+      <meta name="author" content="Jzon" />
+      <Script src="/js/jquery-2.1.3.min.js" />
+
+      <link rel="shortcut icon" href="favicon.ico" />
+      <link rel="stylesheet" href="/css/normalize.css" type="text/css" />
+      <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" />
+      <link rel="stylesheet" href="/css/owl.carousel.css" type="text/css" />
+      <link rel="stylesheet" href="/css/magnific-popup.css" type="text/css" />
+      <link rel="stylesheet" href="/css/main.css" type="text/css" />
+      <Script src="/js/modernizr.custom.js" />
+      <div className="lm-animated-bg" />
+      <div className="preloader">
+        <div className="preloader-animation">
+          <div className="preloader-spinner" />
+        </div>
+      </div>
+      <div className="page-scroll">
+        <div id="page_container" className="page-container bg-move-effect" data-animation="transition-flip-in-right">
+          <header id="site_header" className="header">
+            <div className="header-content clearfix">
+              <div className="text-logo">
+                <a href="index.html">
+                  <div className="logo-symbol">J</div>
+                  <div className="logo-text">Phạm Đức Thanh</div>
+                </a>
+              </div>
+              <div className="site-nav mobile-menu-hide">
+                <ul className="leven-classic-menu site-main-menu">
+                  <li className="menu-item">
+                    <a href="https://card1s.vn">CARD1S.VN</a>
+                  </li>
+                  <li className="menu-item">
+                    <a href="https://jzontech.asia">JZONTECH.ASIA</a>
+                  </li>
+                  <li className="menu-item menu-item-has-children">
+                    <a href="index.html">THÔNG TIN LIÊN HỆ</a>
+                    <ul className="sub-menu">
+                      <li className="menu-item">
+                        <a href="https://zalo.me/0966142061">Zalo / SĐT</a>
+                      </li>
+                      <li className="menu-item">
+                        <a href="https://facebook.com/ducthanhit">Facebook</a>
+                      </li>
+                      <li className="menu-item">
+                        <a href="https://t.me/cuteboiz999">Telegram</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <a className="menu-toggle mobile-visible">
+                <i className="fa fa-bars" />
+              </a>
+            </div>
+          </header>
+          <div id="main" className="site-main">
+            <div id="main-content" className="single-page-content">
+              <div id="primary" className="content-area">
+                <div id="content" className="page-content site-content single-post" role="main">
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12">
+                      <div className="home-content">
+                        <div className="row flex-v-align">
+                          <div className="col-sm-12 col-md-5 col-lg-5">
+                            <div className="home-photo">
+                              <div className="hp-inner" style={{ backgroundImage: 'url(https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/268958507_1746155005578706_3262003589857732831_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=YnSXecpWqUAAX8cUfGD&_nc_oc=AQlzhEBDTLpKD65hE-nBkVR0iEOijjy97DTI4jSIGGQQia25YCkFCYp06Dio6mRtxw8&_nc_ht=scontent.fsgn13-2.fna&oh=00_AT8Dl_eQ9sYeI-YdJuP7fKyiuPSewlg4ay8saAbmciz3eg&oe=61E4FE94)' }} />
+                            </div>
+                          </div>
+                          <div className="col-sm-12 col-md-7 col-lg-7">
+                            <div className="home-text hp-left">
+                              <div className="owl-carousel text-rotation">
+                                <div className="item">
+                                  <h4>Cyber Security</h4>
+                                </div>
+                                <div className="item">
+                                  <h4>Website Developer</h4>
+                                </div>
+                              </div>
+                              <h1>Phạm Đức Thanh</h1>
+                              <p>
+                              / Jz0nTeaM x JzonTech \ <br/>
+                              Software - Website - Security <br/> <br/>
+
+                              🤓 Learn then remove "L"
+                              </p>
+                              <div className="home-buttons">
+                                <a href="https://facebook.com/ducthanhit" target="_blank" className="btn btn-primary">Facebook</a>
+                                <a href="https://t.me/cuteboiz999" target="_blank" className="btn btn-secondary">Telegram</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12">
+                      <div className="p-50" />
+                      <div className="block-title">
+                        <h2>Công việc hiện tại</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-6">
+                      <div className="info-list-w-icon">
+                        <div className="info-block-w-icon">
+                          <div className="ci-icon">
+                            <i className="linecons linecons-pen" />
+                          </div>
+                          <div className="ci-text">
+                            <h4>Viết nội dung quảng cáo</h4>
+                            <p>
+                              Với trình độ văn học đạt kết quả xuất xắc thì viết nội dung quảng cáo chỉ là 1 phần kỹ năng.
+                            </p>
+                          </div>
+                        </div>
+                        <div className="info-block-w-icon">
+                          <div className="ci-icon">
+                            <i className="linecons linecons-display" />
+                          </div>
+                          <div className="ci-text">
+                            <h4>Website Developer</h4>
+                            <p>
+                              Nhà phát triển kiêm lau dọn máy chủ + CSKH tại Jzon Tech
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-6">
+                      <div className="info-list-w-icon">
+                        <div className="info-block-w-icon">
+                          <div className="ci-icon">
+                            <i className="linecons linecons-shop" />
+                          </div>
+                          <div className="ci-text">
+                            <h4>Doanh nhân</h4>
+                            <p>
+                              Doanh nhân thành đạt với hơn 10 shop bán đồ chơi và dép lào toàn quốc.
+                            </p>
+                          </div>
+                        </div>
+                        <div className="info-block-w-icon">
+                          <div className="ci-icon">
+                            <i className="linecons linecons-megaphone" />
+                          </div>
+                          <div className="ci-text">
+                            <h4>Marketing đỉnh cao</h4>
+                            <p>
+                              Jzon Tech hiện đang phục vụ hơn 100 bạn trẻ quảng cáo shop bán acc "Phi Phai" đứng đầu xu hướng.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-12">
+                      <div className="block-title">
+                        <h2>Sự thật ít ai biết</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-3">
+                      <div className="lm-info-block gray-default">
+                        <i className="linecons linecons-heart" />
+                        <h4>Tấm lòng nhân ái</h4>
+                        <span className="lm-info-block-value">98%</span>
+                        <span className="lm-info-block-text" />
+                      </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-3">
+                      <div className="lm-info-block gray-default">
+                        <i className="linecons linecons-clock" />
+                        <h4>Thời gian làm việc</h4>
+                        <span className="lm-info-block-value">24/24</span>
+                        <span className="lm-info-block-text" />
+                      </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-3">
+                      <div className="lm-info-block gray-default">
+                        <i className="linecons linecons-star" />
+                        <h4>Sao bé ngoan</h4>
+                        <span className="lm-info-block-value">999</span>
+                        <span className="lm-info-block-text" />
+                      </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-3">
+                      <div className="lm-info-block gray-default">
+                        <i className="linecons linecons-cup" />
+                        <h4>Cà phê tiêu thụ</h4>
+                        <span className="lm-info-block-value">1,286</span>
+                        <span className="lm-info-block-text" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p-40" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer className="site-footer clearfix">
+            <div className="footer-social">
+              <ul className="footer-social-links">
+                <li>
+                  <a href="https://t.me/cuteboiz999" target="_blank">Telegram</a>
+                </li>
+                <li>
+                  <a href="https://facebook.com/ducthanhit" target="_blank">Facebook</a>
+                </li>
+                <li>
+                  <a href="https://instagram.com/ducthanh.dev" target="_blank">Instagram</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-copyrights">
+              <p>© 2022 All rights reserved. Jzon Tech.</p>
+            </div>
+          </footer>
+        </div>
+      </div>
+
+      <Script src="/js/imagesloaded.pkgd.min.js" />
+      <Script src="/js/bootstrap.min.js" />
+      <Script src="/js/jquery.shuffle.min.js" />
+      <Script src="/js/masonry.pkgd.min.js" />
+      <Script src="/js/owl.carousel.min.js" />
+      <Script src="/js/jquery.magnific-popup.min.js" />
+      <Script src="/js/jquery.googlemap.js" />
+      <Script src="/js/validator.js" />
+      <Script src="/js/main.js" />
+    </div>
+  )
+}
